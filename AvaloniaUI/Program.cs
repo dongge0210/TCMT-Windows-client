27bc072a7201c95c99a8a6aebdfdbb2ca04cd8ff
Huang Zhaoming <116700814+dongge0210@users.noreply.github.com>
2026-05-02 01:16:04 +0800

    fix: configure Serilog file sink in Program.cs

--- AvaloniaUI/Program.cs
+++ AvaloniaUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Serilog;
 using System;
 
 namespace AvaloniaUI
@@ -9,8 +10,24 @@ namespace AvaloniaUI
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("logs/tcmt-.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7)
+                .CreateLogger();
+            try
+            {
+                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
